e38bbc99b6d6ff61cfc96129c8aab466443abc99
Bejowe <fast.pfeil@googlemail.com>
2019-08-19 15:57:14 +0200

    Add static constant File for Company data

---
+++ src/functional_components/Impressum/content.js
@@ -0,0 +1,19 @@
+const COMPANY = {
+    name: 'Owntech',
+    email: 'fakemail@test.de',
+    street: 'Puschkinstraße',
+    houseNumber: '31',
+    city: 'Fürstenwalde',
+    countryCode: '15517',
+    leader: {
+        name: 'Ludwig Schubert',
+        email: 'ludwig@x.de',
+        street: 'Lange Straße',
+        houseNumber: '12',
+        city: 'Berlin',
+        countryCode: '13187',
+    },
+    telefone: '01764829384',
+};
+
+export default COMPANY;
