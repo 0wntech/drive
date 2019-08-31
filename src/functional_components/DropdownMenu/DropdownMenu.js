feae311d44babf10cc1fe5e51d0d7c18712256e4
Bejow <fast.pfeil@googmail.com>
2019-08-31 11:48:38 +0200

    speed up dropdown

--- src/functional_components/DropdownMenu/DropdownMenu.js
+++ src/functional_components/DropdownMenu/DropdownMenu.js
@@ -3,11 +3,8 @@ import PropTypes from 'prop-types';
 import SvgDropdown from '../../assets/svgIcons/Dropdown';
 import styles from './DropdownMenu.module.css';
 import classNames from 'classnames';
-import useHeight from '../../hooks/useHeight';
 
 function DropdownMenu({ options, renderOption, isExpanded, setExpanded }) {
-    const [ref, height] = useHeight();
-    console.log('height', height);
     const defaultRender = (option) => {
         return (
             <div onClick={option.onClick} className={styles.option}>
@@ -27,8 +24,6 @@ function DropdownMenu({ options, renderOption, isExpanded, setExpanded }) {
                 className={classNames(styles.dropdownContent, {
                     [styles.hide]: !isExpanded,
                 })}
-                ref={ref}
-                style={{ bottom: -height }}
             >
                 {renderOption
                     ? options.map((option) => (
