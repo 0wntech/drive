a61f0c675505cd446177975eeb1ac91e596e32bb
Bejow <fast.pfeil@googmail.com>
2019-09-01 18:45:37 +0200

    Simplify Component File

--- src/functional_components/File/File.js
+++ src/functional_components/File/File.js
@@ -14,102 +14,68 @@ export default function File({
     const labelFragments = encodeURIComponent(label).split('.');
     const isImage =
         imageTypes.indexOf(labelFragments[labelFragments.length - 1]) > -1;
-    if (isImage) {
-        return (
-            <MenuProvider
-                className={classNames(styles.gridCell, {
-                    [styles.selected]: selectedItem,
-                })}
-                id={label + 'contextmenu'}
-            >
-                <div
-                    className={styles.container}
-                    style={selectedItem ? { opacity: 0.5 } : undefined}
-                    onClick={onClick}
-                >
-                    <div className={styles.innerContainer}>
-                        <div className={styles.iconContainer}>
-                            <img
-                                className={styles.thumbnail}
-                                src={currPath + label}
-                            />
-                            <img className={styles.icon} src={image} />
-                        </div>
-                    </div>
-                </div>
-                <div className={styles.label}>{label}</div>
-                <Menu className={styles.contextMenu} id={label + 'contextmenu'}>
-                    {contextMenuOptions &&
-                        contextMenuOptions.map((option, index) => (
-                            <Item
-                                disabled={option.disabled}
-                                key={index + option.label}
-                                onClick={
-                                    !option.disabled
-                                        ? () => {
-                                              option.onClick(
-                                                  currPath +
-                                                      encodeURIComponent(label)
-                                              );
-                                          }
-                                        : undefined
-                                }
-                                className={classNames(styles.contextItem, {
-                                    [styles.disabled]: option.disabled,
-                                })}
-                            >
-                                <div>{option.label}</div>
-                            </Item>
-                        ))}
-                </Menu>
-            </MenuProvider>
-        );
-    } else {
-        return (
-            <MenuProvider
-                className={classNames(styles.gridCell, {
-                    [styles.selected]: selectedItem,
-                })}
-                id={label + 'contextmenu1'}
-            >
-                <div
-                    className={styles.container}
-                    style={selectedItem ? { opacity: 0.5 } : undefined}
-                    onClick={onClick}
-                >
-                    <div className={styles.innerContainer}>
+
+    const renderFile = () => {
+        if (isImage) {
+            return (
+                <div className={styles.innerContainer}>
+                    <div className={styles.iconContainer}>
+                        <img
+                            className={styles.thumbnail}
+                            src={currPath + label}
+                        />
                         <img className={styles.icon} src={image} />
                     </div>
                 </div>
-                <div className={styles.label}>{label}</div>
-                <Menu
-                    className={styles.contextMenu}
-                    id={label + 'contextmenu1'}
-                >
-                    {contextMenuOptions &&
-                        contextMenuOptions.map((option, index) => (
-                            <Item
-                                disabled={option.disabled}
-                                key={index + option.label}
-                                onClick={
-                                    !option.disabled
-                                        ? () => {
-                                              option.onClick(
-                                                  currPath +
-                                                      encodeURIComponent(label)
-                                              );
-                                          }
-                                        : undefined
-                                }
-                                className={classNames(styles.contextItem, {
-                                    [styles.disabled]: option.disabled,
-                                })}
-                            >
-                                <div>{option.label}</div>
-                            </Item>
-                        ))}
-                </Menu>
-            </MenuProvider>
-        );
-    }
+            );
+        } else {
+            return (
+                <div className={styles.innerContainer}>
+                    <img className={styles.icon} src={image} />
+                </div>
+            );
+        }
+    };
+
+    return (
+        <MenuProvider
+            className={classNames(styles.gridCell, {
+                [styles.selected]: selectedItem,
+            })}
+            id={label + 'contextmenu'}
+        >
+            <div
+                className={styles.container}
+                style={selectedItem ? { opacity: 0.5 } : undefined}
+                onClick={onClick}
+            >
+                {renderFile()}
+            </div>
+            <div className={styles.label}>{label}</div>
+            <Menu className={styles.contextMenu} id={label + 'contextmenu'}>
+                {contextMenuOptions &&
+                    contextMenuOptions.map((option, index) => (
+                        <Item
+                            disabled={option.disabled}
+                            key={index + option.label}
+                            onClick={
+                                !option.disabled
+                                    ? () => {
+                                          option.onClick(
+                                              currPath +
+                                                  encodeURIComponent(label)
+                                          );
+                                      }
+                                    : undefined
+                            }
+                            className={classNames(styles.contextItem, {
+                                [styles.disabled]: option.disabled,
+                            })}
+                        >
+                            <div>{option.label}</div>
+                        </Item>
+                    ))}
+            </Menu>
+        </MenuProvider>
+    );
 }
