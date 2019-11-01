c51009d632f6cec963f79c8a20ffa63f05148001
Bejow <fast.pfeil@googmail.com>
2019-11-01 12:05:14 +0100

    Create new useRreducer

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,5 +1,7 @@
 import { combineReducers } from 'redux';
 import appReducer from './AppReducer';
+import userReducer from './UserReducer';
 export default combineReducers({
     app: appReducer,
+    user: userReducer,
 });
