83c834b715a8ca4ce36bc86bc5b2cfc26ed2f894
ludwigschubi <ludwigschubi1@gmail.com>
2019-11-25 17:45:43 +0100

    Add fix for Drive test

--- src/stateful_components/Drive/Drive.test.js
+++ src/stateful_components/Drive/Drive.test.js
@@ -2,7 +2,7 @@ import React from 'react';
 import Drive from './Drive.js';
 import TestRenderer from 'react-test-renderer';
 import { withRouter } from 'react-router-dom';
-import appReducer from '../../reducers/AppReducer.js';
+import appReducer from '../../reducers/appReducer.js';
 import { FETCH_CURRENT_ITEM_SUCCESS } from '../../actions/types';
 
 describe('Drive', () => {
