f437d3ae03290469795a6dbd6cc3269dc88c5ea2
ludwigschubi <ludwigschubi1@gmail.com>
2019-12-11 18:07:09 +0100

    Remove logs + fix tests

--- jest-puppeteer.config.js
+++ jest-puppeteer.config.js
@@ -1,11 +1,16 @@
 module.exports = {
     launch: {
-        headless: process.env.NODE_ENV === '',
+        headless: false,
         slowMo: 100,
         userDataDir: './user_data',
         defaultViewport: {
             width: 1920,
             height: 1024,
         },
+    },
+    server: {
+        command: 'BROWSER=none npm start',
+        port: '3000',
+        usedPortAction: 'ignore',
     },
 };
