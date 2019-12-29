27d340f627d00cca24c3902070ef19f59c51105c
Bejowe <fast.pfeil@googlemail.com>
2019-12-29 17:23:02 +0100

    Add remove app reducer functionality

--- src/reducers/userAppReducer.js
+++ src/reducers/userAppReducer.js
@@ -2,6 +2,9 @@ import {
     FETCH_APPS,
     FETCH_APPS_SUCCESS,
     FETCH_APPS_FAILURE,
+    REMOVE_APP,
+    REMOVE_APP_SUCCESS,
+    REMOVE_APP_FAILURE,
 } from '../actions/types';
 
 const INITIAL_STATE = {
@@ -17,6 +20,12 @@ export default (state = INITIAL_STATE, { type, payload }) => {
         case FETCH_APPS_SUCCESS:
             return { ...state, apps: payload, loadApps: false };
         case FETCH_APPS_FAILURE:
+            return { ...state, loadApps: false, error: payload };
+        case REMOVE_APP:
+            return { ...state, loadApps: true };
+        case REMOVE_APP_SUCCESS:
+            return { ...state, loadApps: false, apps: payload };
+        case REMOVE_APP_FAILURE:
             return { ...state, loadApps: false, error: payload };
         default:
             return state;
