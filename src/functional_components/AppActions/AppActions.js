368acb584a113990805421e944c1bb04f20bb7a2
Bejowe <fast.pfeil@googlemail.com>
2020-01-05 17:17:19 +0100

    Grey out unused buttons

--- src/functional_components/AppActions/AppActions.js
+++ src/functional_components/AppActions/AppActions.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import styles from './AppActions.module.css';
-
-import Alert from '../../assets/svgIcons/Alert';
+import classNames from 'classnames';
+// import Alert from '../../assets/svgIcons/Alert';
 import Arrow from '../../assets/svgIcons/ArrowRight';
 import Folder from '../../assets/svgIcons/Folder';
 import Settings from '../../assets/svgIcons/Settings';
@@ -15,18 +15,24 @@ const AppActions = ({
 }) => {
     return (
         <div className={styles.container}>
-            {!alert ? (
+            {/* {!alert ? (
                 <div className={styles.alertIcon}>
                     <Alert viewBox="0 0 25 25" width={17} height={17} />
                 </div>
-            ) : null}
+            ) : null} */}
             <div className={styles.icon} onClick={onArrowClick}>
                 <Arrow viewBox="0 0 21 21" width={17} height={17} />
             </div>
-            <div className={styles.icon} onClick={onFolderClick}>
+            <div
+                className={classNames(styles.disabled, styles.icon)}
+                onClick={onFolderClick}
+            >
                 <Folder viewBox="0 0 21 21" width={17} height={17} />
             </div>
-            <div className={styles.icon} onClick={onSettingsClick}>
+            <div
+                className={classNames(styles.disabled, styles.icon)}
+                onClick={onSettingsClick}
+            >
                 <Settings viewBox="0 0 25 25" width={17} height={17} />
             </div>
         </div>
